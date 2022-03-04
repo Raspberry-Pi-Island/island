89aeb6f16cb0f4447961c763fe817f5e65cba4ed
elleaech <lohan.chuan123@gmail.com>
2022-03-04 17:51:43 -0300

    CL parameters adjustment

--- rpi_island.py
+++ rpi_island.py
@@ -9,12 +9,11 @@ import argparse
 def get_cl_parameters() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("client_link_t", help="external link type")
-    parser.add_argument("client_addr", help="external ip address")
-    parser.add_argument("client_addr_network_bytes", help="netmask bytes number")
-    parser.add_argument("gateway_link_t", help="internal link type")
-    parser.add_argument("gateway_addr", help="internal gateway ip address")
-    parser.add_argument("gateway_addr_network_bytes", help="netmask bytes number")
+    parser.add_argument("link_t", help="external link type")
+    parser.add_argument("external_addr", help="external ip address")
+    parser.add_argument("external_network_bytes", help="netmask bytes")
+    parser.add_argument("gateway_addr", help="raspberry as a gateway address")
+    parser.add_argument("gateway_network_bytes", help="netmask bytes")
 
     args = parser.parse_args()
 
@@ -23,12 +22,11 @@ def get_cl_parameters() -> argparse.Namespace:
 
 def print_cl_parameters(params: argparse.Namespace):
     print("-" * 50)
-    print(f"CLIENT LINK TYPE: {params.client_link_t}")
-    print(f"CLIENT ADDRESS: {params.client_addr}")
-    print(f"CLIENT NETMASK BYTES: {params.client_addr_network_bytes}")
-    print(f"GATEWAY LINK TYPE: {params.gateway_link_t}")
-    print(f"GATEWAY ADDRESS: {params.gateway_addr}")
-    print(f"GATEWAY NETMASK BYTES: {params.gateway_addr_network_bytes}")
+    print(f"EXTERNAL LINK TYPE: {params.link_t}")
+    print(f"EXTERNAL IP ADDRESS: {params.external_addr}")
+    print(f"EXTERNAL NETMASK BYTES: {params.external_network_bytes}")
+    print(f"RASPBERRY AS A GATEWAY ADDRESS: {params.gateway_addr}")
+    print(f"GATEWAY NETMASK BYTES: {params.gateway_network_bytes}")
     print("-" * 50)
 
 
@@ -55,15 +53,15 @@ def init_address(addr_network_bytes: int, address_as_str: str):
 def set_network(
     client_link_t: str,
     gateway_link_t: str,
-    client_addr: RTIpv4Address,
+    external_addr: RTIpv4Address,
     gateway_addr: RTIpv4Address,
 ) -> bool:
     print("\nSETTING NETWORK INTERFACES:")
 
-    if client_addr != None and gateway_addr != None:
+    if external_addr != None and gateway_addr != None:
         conf_directory = RTPath(Path("/", "etc", "network", "interfaces.d"))
 
-        conf_directory.append(RTNetFile(client_addr, client_link_t, True))
+        conf_directory.append(RTNetFile(external_addr, client_link_t, True))
         conf_directory.append(RTNetFile(gateway_addr, gateway_link_t, False))
 
         conf_directory.go()
@@ -116,20 +114,29 @@ def set_firewall() -> bool:
 
 
 def main(params: argparse.Namespace) -> int:
-    client_addr = init_address(params.client_addr_network_bytes, params.client_addr)
-    gateway_addr = init_address(params.gateway_addr_network_bytes, params.gateway_addr)
+    gateway_link_t = ""
+
+    external_addr = init_address(params.external_network_bytes, params.external_addr)
+    gateway_addr = init_address(params.gateway_network_bytes, params.gateway_addr)
+
+    if params.link_t.startswith("wlan"):
+        interface_number: str = params.link_t.split("wlan")[1]
+        gateway_link_t = f"eth{interface_number}"
+    elif params.link_t.startswith("eth"):
+        interface_number: str = params.link_t.split("eth")[1]
+        gateway_link_t = f"wlan{interface_number}"
 
     net_is_configured: bool = set_network(
-        params.client_link_t,
-        params.gateway_link_t,
-        client_addr,
+        params.link_t,
+        gateway_link_t,
+        external_addr,
         gateway_addr,
     )
 
     if (
         net_is_configured
         and disable_dhcp_client()
-        and set_dhcp_server(gateway_addr, params.gateway_link_t)
+        and set_dhcp_server(gateway_addr, gateway_link_t)
         and set_firewall()
     ):
         return 0
@@ -141,13 +148,9 @@ if __name__ == "__main__":
     params = get_cl_parameters()
     print_cl_parameters(params)
 
-    if params.client_link_t == params.gateway_link_t:
-        print("link_t cannot be equal!")
-        exit(-1)
-
     try:
-        params.client_addr_network_bytes = int(params.client_addr_network_bytes)
-        params.gateway_addr_network_bytes = int(params.gateway_addr_network_bytes)
+        params.external_network_bytes = int(params.external_network_bytes)
+        params.gateway_network_bytes = int(params.gateway_network_bytes)
     except ValueError:
         raise ValueError
 
